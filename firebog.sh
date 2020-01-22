b3a8b5cf9c2d5777629b73fe559ec0494cfb3b4f
jpgpi250 <jpgpi250@gmail.com>
2020-01-22 15:26:20 +0100

    Update firebog.sh

--- firebog.sh
+++ firebog.sh
@@ -3,5 +3,5 @@
 wget https://v.firebog.net/hosts/lists.php?type=nocross -O /home/pi/firebog.list
 while read nocross
 do
-	sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into adlist (address,comment) values (\"$nocross\", 'firebog nocross');"
+	sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into adlist (address,comment,enabled) values (\"$nocross\", 'firebog nocross', 1);"
 	done < /home/pi/firebog.list
