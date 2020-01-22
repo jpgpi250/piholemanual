1731cb4815616fb7bcc0404faf377ff50ea6e38b
jpgpi250 <jpgpi250@gmail.com>
2020-01-22 15:19:21 +0100

    Update regex.sh

--- regex.sh
+++ regex.sh
@@ -3,6 +3,6 @@
 sudo curl https://raw.githubusercontent.com/mmotti/pihole-regex/master/regex.list -o /home/pi/regex.list
 while read regex
 do
-	sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain,type) values (\"$regex\", 3);"
+	sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled) values (\"$regex\", 3, 1);"
 	done < /home/pi/regex.list
 	
