5bd27cda6a87911b62d2e9f3107d0c0d134db031
jpgpi250 <jpgpi250@gmail.com>
2020-01-26 09:40:59 +0100

    Create IPv6check.sh

---
+++ IPv6check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get current IPv6 address
+CURRENT_IPV6_ADDRESS=$(ip -6 a | grep '2a02' | awk -F " " '{gsub("/[0-9]*",""); print $2}')
+
+# read configured IPv6 address from /etc/pihole/setupVars.conf
+file=/etc/pihole/setupVars.conf
+OLD_IPV6_ADDRESS=$(grep 'IPV6_ADDRESS=' "$file" |sed 's/^IPV6_ADDRESS=//')
+
+# read/compare previous IPv6 address from file
+if ! grep -q "$CURRENT_IPV6_ADDRESS" $file; then
+	sed -i.bak  "s/$OLD_IPV6_ADDRESS\b/$CURRENT_IPV6_ADDRESS/g" "$file"
+	{
+		echo from: root 
+		echo to: pi
+		echo subject: pihole IPv6 address change
+		echo
+		cat /etc/pihole/setupVars.conf | grep 'ADDRESS'
+	} | sudo sendmail -d -t pi
+	/usr/local/bin/pihole updateGravity
+fi
