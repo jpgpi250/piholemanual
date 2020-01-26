3d0c01aad44b8322364a56327c03b7a8131adb1c
jpgpi250 <jpgpi250@gmail.com>
2020-01-26 09:54:51 +0100

    Create phpliteadmin.sh

---
+++ phpliteadmin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file=phpLiteAdmin_v1-9-8-2.zip
+mkdir -p phpLiteAdmin
+cd phpLiteAdmin
+wget https://bitbucket.org/phpliteadmin/public/downloads/$file
+unzip $file
+
+file=phpliteadmin.php
+sudo mv /home/pi/phpLiteAdmin/$file /var/www/html/$file
+source=phpliteadmin.config.sample.php
+target=phpliteadmin.config.php
+sudo mv /home/pi/phpLiteAdmin/$source /var/www/html/$target
+
+sudo sed -i "/$directory = '.';/c\$directory = '/etc/pihole';" /var/www/html/$target
+sudo sed -i '/$subdirectories = false;/c\$subdirectories = true;' /var/www/html/$target
+
+sudo apt-get –y install php-mbstring
+sudo service lighttpd stop
+sudo service lighttpd start
