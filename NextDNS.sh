7f4564ff7a36b9456efd194a69cf43c3b16679df
jpgpi250 <jpgpi250@gmail.com>
2020-02-17 18:58:34 +0100

    Update NextDNS.sh

--- NextDNS.sh
+++ NextDNS.sh
@@ -8,7 +8,6 @@ while read domain
 do
 	if ! [[ "$domain" == \#* ]]; then
 		if [ ! -z "$domain" ]; then
-			#echo $domain
 			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
 			sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (type, domain, enabled, comment) values (3, \"$regex\", 1, 'NextDNS CNAME list');"
 			fi
