657a73309ef689ae18dfd581532129b1bc6d6547
jpgpi250 <jpgpi250@gmail.com>
2020-02-17 18:23:56 +0100

    Create NextDNS.sh

---
+++ NextDNS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# https://github.com/nextdns/cname-cloaking-blocklist
+
+file=/home/pi/tmp/domains
+sudo wget https://raw.githubusercontent.com/nextdns/cname-cloaking-blocklist/master/domains -O $file
+
+while read domain
+do
+	if ! [[ "$domain" == \#* ]]; then
+		if [ ! -z "$domain" ]; then
+			#echo $domain
+			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
+			sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (type, domain, enabled, comment) values (3, \"$regex\", 1, 'NextDNS CNAME list');"
+			fi
+		fi
+	done < $file
