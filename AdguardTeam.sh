f310a8a9a5fc7731e54144b488626cc4a3dced1f
jpgpi250 <jpgpi250@gmail.com>
2021-03-05 15:35:46 +0100

    updated Fri 05 Mar 2021 15:33 CEST

---
+++ AdguardTeam.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# https://github.com/AdguardTeam/cname-trackers
+
+file=/home/pi/tmp/cloaked-trackers.json
+sudo wget https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/script/src/cloaked-trackers.json -O $file
+
+IFS=[,]
+while read line; do
+	domains=( ${line} )
+	for domain in "${domains[@]}"; do 
+		if [ ! -z "$domain" ]; then
+			#echo $domain
+			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
+			sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (type, domain, enabled, comment) values (3, \"$regex\", 1, 'AdguardTeam CNAME list');"
+		fi	done
+done < <(jq --raw-output "map(\"\(.domains)\")|.[]" < /home/pi/tmp/cloaked-trackers.json < ${file} | tr -d '[]"')
+
+/usr/local/bin/pihole restartdns reload-lists
