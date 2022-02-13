d4f188ebd4b77a31a3bd28da22028ec3fbd272cc
jpgpi250 <jpgpi250@gmail.com>
2022-02-13 15:24:26 +0100

    updated Sun 13 Feb 2022 15:24 CET

--- googleads.sh
+++ googleads.sh
@@ -11,23 +11,23 @@
 # the effect of the new group will NOT be noticable if you dont remove them.
 
 # whitelist entries
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googleadservices.com', 0, 1, 'allow google ads');"
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('dartsearch.net', 0, 1, 'allow google ads');"
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googletagmanager.com', 0, 1, 'allow google ads');"
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googletagservices.com', 0, 1, 'allow google ads');"
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('ad.doubleclick.net', 0, 1, 'allow google ads');"
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (domain, type, enabled, comment) values ('clickserve.dartsearch.net', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googleadservices.com', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('dartsearch.net', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googletagmanager.com', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('www.googletagservices.com', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('ad.doubleclick.net', 0, 1, 'allow google ads');"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (domain, type, enabled, comment) values ('clickserve.dartsearch.net', 0, 1, 'allow google ads');"
 
 # group
-sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into 'group' (enabled, name, description) values ( 1, 'googleads', 'devices with google ads');"
-GROUP_ID="$(sudo sqlite3 /etc/pihole/gravity.db "SELECT id FROM 'group' WHERE name = 'googleads';")"
+sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into 'group' (enabled, name, description) values ( 1, 'googleads', 'devices with google ads');"
+GROUP_ID="$(sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "SELECT id FROM 'group' WHERE name = 'googleads';")"
 
 # assign whitelist entries to group
-mapfile -t DOMAIN_ID < <(sudo sqlite3 /etc/pihole/gravity.db "SELECT id FROM 'domainlist' WHERE comment = 'allow google ads';")
+mapfile -t DOMAIN_ID < <(sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "SELECT id FROM 'domainlist' WHERE comment = 'allow google ads';")
 lenArray=${#DOMAIN_ID[@]}
 
 for (( i=0; i<$lenArray; i++ )); do
-	sudo sqlite3 /etc/pihole/gravity.db "update 'domainlist_by_group' set group_id=\"$GROUP_ID\" WHERE domainlist_id = \"${DOMAIN_ID[$i]}\";"
+	sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "update 'domainlist_by_group' set group_id='$GROUP_ID' WHERE domainlist_id = '${DOMAIN_ID[$i]}';"
 done
 
 pihole restartdns reload-lists
