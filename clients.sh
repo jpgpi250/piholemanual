db59d8072e77c72e19bab62690a2695f72997c3e
jpgpi250 <jpgpi250@gmail.com>
2022-02-13 15:22:35 +0100

    updated Sun 13 Feb 2022 15:22 CET

--- clients.sh
+++ clients.sh
@@ -4,7 +4,7 @@ while read client
 do
 	IP="$(echo $client | cut --delimiter " " --fields 1)"
 	COMMENT="$(echo $client | grep -o '[^ ]*$')"
-	sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into client (ip, comment) values (\"$IP\", \"$COMMENT\");"
+	sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into client (ip, comment) values ('$IP', '$COMMENT');"
 	done < /etc/localdns.list
 
 pihole restartdns reload-lists  
