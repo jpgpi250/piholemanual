06ddbbf6f18b5548bfbd8a6186b0306020b991a2
jpgpi250 <jpgpi250@gmail.com>
2022-02-13 15:21:36 +0100

    updated Sun 13 Feb 2022 15:21 CET

--- AdguardTeam.sh
+++ AdguardTeam.sh
@@ -16,9 +16,8 @@ while read line; do
 	domains=( ${line} )
 	for domain in "${domains[@]}"; do 
 		if [ ! -z "$domain" ]; then
-			#echo $domain
 			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
-			sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (type, domain, enabled, comment) values (3, \"$regex\", 1, 'AdguardTeam CNAME list');"
+			sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (type, domain, enabled, comment) values (3, '$regex', 1, 'AdguardTeam CNAME list');"
 		fi
 	done
 done < <(jq --raw-output "map(\"\(.domains)\")|.[]" < /home/pi/cloaked-trackers.json < ${file} | tr -d '[]"')
