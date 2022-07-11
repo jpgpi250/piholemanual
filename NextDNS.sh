f798e8a58aa7e242989992e5a8a11033ad23aca1
jpgpi250 <jpgpi250@gmail.com>
2022-07-11 10:49:20 +0200

    updated Mon 11 Jul 2022 10:49 CEST

--- NextDNS.sh
+++ NextDNS.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 # https://github.com/nextdns/cname-cloaking-blocklist
+#
+# If you want to report a problem with this script, do NOT create a topic on the pihole forum!
+# Report the issue on https://github.com/jpgpi250/piholemanual/issues
+#
+
+gravitydb="/etc/pihole/gravity.db"
+
+# eye candy / color
+RED='\033[0;91m'
+GREEN='\033[0;92m'
+BLUE='\033[0;94m'
+NC='\033[0m' # No Color
+NOK=" [${RED}!${NC}] "
+INFO=" [${BLUE}i${NC}] "
+
+dbversion=$(pihole-FTL sqlite3 "${gravitydb}" ".timeout = 2000" \
+	"SELECT value FROM 'info' \
+		WHERE property = 'version';")
+if [[ "${dbversion}" != "15" ]]; then
+	echo -e "${NOK}This script was written for gravity database version 15 ${GREEN}(current version: ${dbversion})${NC}."
+	echo -e "${INFO}Open an issue on GitHub (https://github.com/jpgpi250/piholemanual/issues)."
+	exit
+fi
 
 file=/home/pi/domains
 sudo wget https://raw.githubusercontent.com/nextdns/cname-cloaking-blocklist/master/domains -O $file
@@ -9,7 +32,10 @@ do
 	if ! [[ "$domain" == \#* ]]; then
 		if [ ! -z "$domain" ]; then
 			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
-			sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into domainlist (type, domain, enabled, comment) values (3, '$regex', 1, 'NextDNS CNAME list');"
+			sudo pihole-FTL sqlite3 "${gravitydb}" ".timeout = 2000" \
+				"insert or ignore into domainlist \
+					(type, domain, enabled, comment) \
+					values (3, '$regex', 1, 'NextDNS CNAME list');"
 			fi
 		fi
 	done < $file
