b24033717d44cd01090eee19844ea34217db2933
jpgpi250 <jpgpi250@gmail.com>
2023-01-16 16:48:14 +0100

    updated Mon 16 Jan 2023 16:48 CET

--- lighttpdstatus.sh
+++ lighttpdstatus.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# add lighttpd server-status
-file=/etc/lighttpd/external.conf
-if ! grep -q "status.status-url" $file; then
-	echo 'status.status-url = "/server-status"' | sudo tee -a $file
-fi
-file=/etc/lighttpd/lighttpd.conf
-if ! grep -q "mod_status" $file; then
-	sed -i '/"mod_auth",/a\\t"mod_status",' $file
+# fastcgi-php is no longer part of pi-hole since v5.15
+if ! ls -l /etc/lighttpd/conf-enabled/ | grep -q "fastcgi-php"; then
+	sudo lighttpd-enable-mod fastcgi-php
 fi
 
-# check configuration
-# this should echo Syntax OK
+# add lighttpd server-status config file (if it doesn't exist)
+if ! [ -f "/etc/lighttpd/conf-available/51-serverstatus.conf" ]; then
+	{
+		echo "server.modules += ("
+		echo "	\"mod_status\""
+		echo ")"
+		echo "status.status-url = \"/server-status\""
+	} | sudo tee -a /etc/lighttpd/conf-available/51-serverstatus.conf >/dev/null
+
+	# enable config, creates a link in /etc/lighttpd/conf-enabled/
+	sudo lighttpd-enable-mod serverstatus
+fi
+
+# check configuration, this should echo Syntax OK
 lighttpd -t -f /etc/lighttpd/lighttpd.conf
 
 # restart lighttpd
-sudo service lighttpd stop
-sudo service lighttpd start
+sudo service lighttpd force-reload
