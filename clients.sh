0e0205350311dad436fcfab425080b8ea76c7827
jpgpi250 <jpgpi250@gmail.com>
2023-11-28 17:25:19 +0100

    updated Tue 28 Nov 2023 17:25 CET

--- clients.sh
+++ clients.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+#
+# If you want to report a problem with this script, do NOT create a topic on the pihole forum!
+# Report the issue on https://github.com/jpgpi250/piholemanual/issues
+#
+
+gravitydb="/etc/pihole/gravity.db"
+
+# eye candy / color
+RED='\033[0;91m'
+GREEN='\033[0;92m'
+BLUE='\033[0;94m'
+NC='\033[0m' # No Color
+NOK=" [${RED}!${NC}] "
+INFO=" [${BLUE}i${NC}] "
+
+dbversion=$(pihole-FTL sqlite3 "${gravitydb}" ".timeout = 2000" \
+	"SELECT value FROM 'info' \
+		WHERE property = 'version';")
+if [[ "${dbversion}" != "17" ]]; then
+	echo -e "${NOK}This script was written for gravity database version 17 ${GREEN}(current version: ${dbversion})${NC}."
+	echo -e "${INFO}Open an issue on GitHub (https://github.com/jpgpi250/piholemanual/issues)."
+	exit
+fi
 
 while read client
 do
 	IP="$(echo $client | cut --delimiter " " --fields 1)"
 	COMMENT="$(echo $client | grep -o '[^ ]*$')"
 	sudo pihole-FTL sqlite3 "/etc/pihole/gravity.db" "insert or ignore into client (ip, comment) values ('$IP', '$COMMENT');"
-	done < /etc/localdns.list
+	done < /etc/pihole/hosts/localdns.list
 
 pihole restartdns reload-lists  
