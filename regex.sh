060db69a00a3323bcd84a78b7c72d611255ae47c
jpgpi250 <jpgpi250@gmail.com>
2025-04-30 10:23:18 +0200

    updated Wed 30 Apr 2025 10:23 CEST

--- regex.sh
+++ regex.sh
@@ -14,8 +14,6 @@ NC='\033[0m' # No Color
 NOK=" [${RED}!${NC}] "
 INFO=" [${BLUE}i${NC}] "
 
-# This script will work for both v5 and v6, change the value to match your version.
-# gravity database: pi-hole v5 - version = 15, v6 version = 19
 dbversion=$(pihole-FTL sqlite3 "${gravitydb}" ".timeout = 2000" \
 	"SELECT value FROM 'info' \
 		WHERE property = 'version';")
@@ -32,4 +30,4 @@ while read -r regex; do
 	fi
 done < /home/pi/regex.list
 	
-pihole restartdns reload-lists  
+sudo /usr/local/bin/pihole reloadlists  
